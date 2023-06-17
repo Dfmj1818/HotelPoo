9ee0d24f5b829bb10e92a26cdf3e28e3cb99fe32
Dfmj1818 <danieljimenezgg@gmail.com>
2023-06-17 11:29:01 -0500

    Se Añadieron metodos getters y setters a la clase User,Se Creo una lista para Guardar reservas

--- src/Model/User.java
+++ src/Model/User.java
@@ -1,5 +1,43 @@
 package Model;
 
-public class User {
+import java.util.ArrayList;
 
+public class User {
+	private String mail;
+	private String password;
+	private ArrayList<Reserve>reservesList;
+
+	public User(String mail,String password) {
+		this.mail=mail;
+		this.password=password;
+
+	}
+
+	public void addReserveToList(Reserve reserve) {
+		reservesList.add(reserve);
+	}
+
+	public void setUserMail(String mail) {
+		this.mail=mail;
+	}
+
+	public String getUserMail() {
+		return mail;
+	}
+
+	public void setUserPassword(String password) {
+		this.password=password;
+	}
+
+	public String getUserPassword() {
+		return password;
+	}
+	
+	public void setReservesList(ArrayList<Reserve>reservesList) {
+		this.reservesList=reservesList;
+	}
+	
+	public ArrayList<Reserve>getReservesList(){
+		return reservesList;
+	}
 }
