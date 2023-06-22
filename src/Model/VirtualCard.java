f49bd2625aea5fb20fa6e73bd09aec22b9f65411
Dfmj1818 <danieljimenezgg@gmail.com>
2023-06-22 16:28:12 -0500

    Se añadio Clase VirtualCard para Que el Cliente pueda pagar susreservas

--- src/Model/VirtualCard.java
+++ src/Model/VirtualCard.java
@@ -1,5 +1,53 @@
 package Model;
 
-public class VirtualCard {
+import java.time.LocalDate;
 
+public class VirtualCard {
+	
+	private int cardCode;
+	private int funds;
+	private int CCV;
+	private LocalDate expeditionDate;
+	private LocalDate expirationDate;
+	
+	public VirtualCard(int cardCode,int funds,int CCV,LocalDate expeditionDate,LocalDate expirationDate) {
+		this.cardCode=cardCode;
+		this.funds=funds;
+		this.CCV=CCV;
+		this.expeditionDate=expeditionDate;
+		this.expirationDate=expeditionDate;
+	}
+	
+	public void setCardCode(int cardCode) {
+		this.cardCode=cardCode;
+	}
+	
+	public int getCardCode() {
+		return cardCode;
+	}
+
+	public void setFunds(int funds) {
+		this.funds=funds;
+	}
+	
+	public int getFunds() {
+		return funds;
+	}
+	
+	public void setExpeditionDate(LocalDate expeditionDate) {
+		this.expeditionDate=expirationDate;
+	}
+	
+	public LocalDate getExpeditionDate() {
+		return expeditionDate;
+	}
+	
+	public void setExpirationDate(LocalDate expirationDate) {
+		this.expirationDate=expirationDate;
+	}
+	
+	public LocalDate getExpirationDate() {
+		return expirationDate;
+	}
+	
 }
