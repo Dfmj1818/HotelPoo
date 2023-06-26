b9c5981be94427d930009711a411ce1d0cde96c5
Dfmj1818 <danieljimenezgg@gmail.com>
2023-06-26 17:41:13 -0500

    Se Arreglo el metodo generateRandomFunds

--- src/Model/VirtualCard.java
+++ src/Model/VirtualCard.java
@@ -9,13 +9,13 @@ public class VirtualCard {
 	private int funds;
 	private int CCV;
 	private LocalDate expirationDate;
-	private Random randomFunds;
-	
-	public VirtualCard(User user,int cardCode,int CCV,LocalDate expirationDate) {
+
+
+	public VirtualCard(User user,int cardCode,int CCV,LocalDate expirationDate) {	
 		this.cardCode=cardCode;
 		this.funds=generateRandomFunds();
 		this.CCV=CCV;
-	
+
 	}
 
 	public void setCardCode(int cardCode) {
@@ -26,34 +26,27 @@ public class VirtualCard {
 		return cardCode;
 	}
 
-	public void setFunds(int funds) {
-		this.funds=funds;
-	}
-
-	public int getFunds() {
-		return funds;
-	}
-	
 	public void setCCV(int CCV) {
 		this.CCV=CCV;
 	}
-	
+
 	public int getCCV() {
 		return CCV;
 	}
-	
+
 	public void setExpirationDate(LocalDate expirationDate) {
 		this.expirationDate=expirationDate;
 	}
-	
+
 	public LocalDate getExpirationDate() {
 		return expirationDate;
 	}
+	//Se hizo Correccion 
+	public int generateRandomFunds() {
+		Random randomFunds=new Random();   
+		return randomFunds.nextInt(500000);
+	}
 
-   public int generateRandomFunds() {
-	   int funds=randomFunds.nextInt(500000);
-	   return funds;
-   }
-   
+
 
 }
